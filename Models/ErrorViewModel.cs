cdb1d7566e6c98f5b3448a3afbb9947882a2aeb3
thomas-jeff06 <64867272+thomas-jeff06@users.noreply.github.com>
2023-05-23 12:30:03 -0300

    Adicionar arquivos de projeto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ManageCollaborator.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
